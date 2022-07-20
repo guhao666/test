a925db827935431b53ef49784fac925f0eb12880
guhao <guhao0222@163.com>
2022-07-20 22:07:33 +0800

    second commit

--- com/yonyou/git/GitTest.java
+++ com/yonyou/git/GitTest.java
@@ -3,5 +3,6 @@ package com.yonyou.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
